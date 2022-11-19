ef13f99e0e37e4d4d446e5f77c472aef0e51dda2
Vansh Gupta <vg224@cornell.edu>
2022-11-18 20:14:13 -0500

    Update preprocess.R

--- utils/preprocess.R
+++ utils/preprocess.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 
+# Import Libraries
 library(readstata13)
 library(glmnet)
 library(randomForest)
@@ -21,3 +22,23 @@ library(lfe)
 library(sandwich)
 library(AER)
 library(ggpubr)
+
+# Load Data
+setwd("G:/Other computers/My PC/Desktop/Cornell/Research/Thesis/Schools Analysis/thesis-brazil-schools-analysis")
+schools <- read.csv("data/BrazilEduPanel_School.csv")
+
+# Name Columns
+cols <- names(schools)
+#cb_startups = cb_startups %>%
+#  mutate(early_stage_amount_log = log(early_stage_amount),
+#         bearly_stage_has_vc = as.double(cb_startups$bearly_stage_has_vc),
+#         early_stage_bin = floor(log10(early_stage_amount)*2))
+
+#ml_vars <- c(cols[grepl("^I.*", cols)],
+             #cols[grepl(".*_ed.*", cols)],
+             #cols[grepl("^ed.*", cols)],"early_stage_bin")
+#sum(is.na(cb_startups[,ml_vars]))
+
+#colSums(is.na(cb_startups[,ml_vars]))
+#dim(cb_startups)
+
